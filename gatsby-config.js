042fff3938ac7e3274ad48f43224b521d68bf27a
Bart <bartek@redly.pl>
2023-11-15 15:33:10 +0100

    gtag was added

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,26 +2,38 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `Furnish Apart`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-styled-components", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `Furnish Apart`,
+        siteUrl: `https://www.yourdomain.tld`,
     },
-    __key: "images"
-  }]
-};
+    plugins: [
+        "gatsby-plugin-styled-components",
+        {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+                trackingIds: ["G-78MT0L56SL"],
+                pluginConfig: {
+                    head: true,
+                },
+            },
+        },
+        "gatsby-plugin-image",
+        "gatsby-plugin-sitemap",
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                icon: "src/images/icon.png",
+            },
+        },
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+    ],
+}
